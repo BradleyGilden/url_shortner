c46f472a200afc750f29326cb64b4a1b290358cb
BradleyGilden <bradleygilden@gmail.com>
2024-02-10 17:50:20 +0200

    set up files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+// body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
+// use ejs as template engine
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+
+  res.status(200).render('index')
+})
