ce4bb8c021dbd16a1eb72deebb284452b617519f
BradleyGilden <bradleygilden@gmail.com>
2024-02-11 16:47:11 +0200

    styling fix

--- public/main.js
+++ public/main.js
@@ -3,12 +3,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const slider = document.querySelector("#slider")
   const time = document.querySelector("#time");
   const rid = document.querySelector("#rid");
+  const copy = document.querySelector("#copy");
+  const url = document.querySelector("#urlform div p");
+
+  copy.addEventListener("click", () => {
+    const ridText = url.textContent;
+    navigator.clipboard.writeText(ridText)
+    .then(() => {
+      copy.textContent = "copied!";
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  })
 
   slider.addEventListener("input", () => {
     time.textContent = slider.value;
   })
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
+    copy.textContent = "copy";
     // get form data
     const data = new URLSearchParams(new FormData(form));
     const res = await fetch("/shorten", {
